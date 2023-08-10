bfa51c67b65a582896cbda7f981af2b9dc6d3304
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-08-09 22:20:26 -0700

    completed Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom';
 import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}> 
       <Route index element={<IndexPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   )
@@ -17,4 +19,4 @@ function App() {
 
 export default App
 
-//left off at 33:30
+//left off at 43:03
