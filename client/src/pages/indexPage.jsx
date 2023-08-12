14a11b882edba6347b3042193d9a77e1db874b79
Rene A Larson <122948946+ReneLarson1990@users.noreply.github.com>
2023-08-12 11:56:33 -0700

    changes to index page

--- client/src/pages/indexPage.jsx
+++ client/src/pages/indexPage.jsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom";
+import Header from "../Header";
 
 export default function IndexPage() {
-  return (
-    <div>
-      index page here 
-    </div>
-  );
+    return (
+        <div>
+            index page here
+      </div>
+    )
 }
