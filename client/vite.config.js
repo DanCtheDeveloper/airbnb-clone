2674c222f9474102f1b685dd29282dadac33ffe8
JonsCodes <johnalyashai@me.com>
2023-08-17 17:44:42 -0700

    attempting proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
-module.exports = {
-  server: {
-    proxy: {
-      // When your frontend makes requests to /api,
-      // Vite will proxy the request to the backend server.
-      '/api': 'http://localhost:4000', // Replace with your backend URL
-    },
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // vite.config.js
+// module.exports = {
+//   server: {
+//     proxy: {
+//       // When your frontend makes requests to /api,
+//       // Vite will proxy the request to the backend server.
+//       '/api': 'http://localhost:4000', // Replace with your backend URL
+//     },
+//   },
+// };
